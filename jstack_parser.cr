b44096c79082baa747fd2c83d830a78a328106fc
rogerdpack <rogerpack2005@gmail.com>
2017-08-30 11:14:32 -0600

    new optionsgit diff

---
+++ jstack_parser.cr
@@ -0,0 +1,39 @@
+all = [] of Array(String)
+
+if ARGV.size != 2 || ARGV.includes?("-h") || ARGV.includes?("--help")
+  puts  "syntax: <jstack_filename> <criteria>
+  criteria options: size > X, exclude ThisString"
+  exit 1
+end
+
+criteria = ARGV[1]
+
+stack = File.read(ARGV[0])
+current = [] of String
+stack.each_line{|l|
+  l = l.gsub("\t", "   ")
+  if l.starts_with?("\"")
+    all << current.clone
+    current.clear # !
+  end
+  current << l
+}
+all = all[1..-1] # ignore first which is the header
+
+if criteria =~ /size > (\d+)/
+  min_size = $1.to_i
+  puts "specifying min size #{min_size}"
+  all = all.select{|stack| stack.size > min_size}
+end
+
+if criteria =~ /exclude (\S+)/ # \S is non whitespace
+  exclude_this = $1
+  puts "excluding #{exclude_this}"
+  all = all.reject{|stack| stack.join.includes?(exclude_this) }
+end 
+
+all.each{|trace|
+  puts trace.join("\n")
+  puts "\n" # follow normal jstack semantics a bit :)
+}
+

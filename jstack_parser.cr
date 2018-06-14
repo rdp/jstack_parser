090d29612b729fb06e3d669ebf4248aff72add2f
rogerdpack <rogerpack2005@gmail.com>
2018-06-14 13:12:31 -0600

    support more than one woot

--- jstack_parser.cr
+++ jstack_parser.cr
@@ -1,38 +1,46 @@
 all = [] of Array(String)
 
-if ARGV.size != 2 || ARGV.includes?("-h") || ARGV.includes?("--help")
-  puts  "syntax: #{PROGRAM_NAME} <jstack_filename> <criteria> (only one supported for now)
+if ARGV.size < 1 || ARGV.includes?("-h") || ARGV.includes?("--help")
+  puts  "syntax: #{PROGRAM_NAME} <jstack_filename> <criteria>
     criteria options: 
       \"size > X\"
-      \"exclude ThisString\""
+      \"exclude ThisString\"
+      ex: jstack_parser jstack_filename \"size > 3\" \"exclude DontWant\"
+"
   exit 1
 end
 
-criteria = ARGV[1]
+criteria = ARGV[1..-1]
 
 stack = File.read(ARGV[0])
-current = [] of String
+
+current_trace = [] of String
 stack.each_line{|l|
   l = l.gsub("\t", "   ")
   if l.starts_with?("\"")
-    all << current.clone
-    current.clear # !
+    all << current_trace.clone
+    current_trace.clear # !
   end
-  current << l.strip
+  current_trace << l
 }
-all = all[1..-1] # ignore first which is the header
 
-if criteria =~ /size > (\d+)/
+all = all[1..-1] # ignore first which is the header "trace"
+
+criteria.each{|criterion|
+
+if criterion =~ /size > (\d+)/
   min_size = $1.to_i
   puts "specifying min size #{min_size}"
   all = all.select{|stack| stack.size > min_size}
 end
 
-if criteria =~ /exclude (\S+)/ # \S is non whitespace
+if criterion =~ /exclude (\S+)/ # \S is non whitespace
   exclude_this = $1
   puts "excluding #{exclude_this}"
   all = all.reject{|stack| stack.join.includes?(exclude_this) }
 end 
+
+}
 
 all.each{|trace|
   puts trace.join("\n")
